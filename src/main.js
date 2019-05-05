3fe6a2d543f0ceb5d55a9efbed351c94ccb18c23
sckoh <soocheng.koh@ifastfinancial.com>
2019-05-05 21:01:42 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
+import { SubmissionError } from 'redux-form';
+
+export const isInvalid = (errors = {}) => !!Object.keys(errors).length;
+
+export const isValid = (errors = {}) => !isInvalid(errors);
+
+export const validateSubmit = (validate, submit) => (formValues, dispatch, props) => {
+  const validated = validate(formValues, props, true);
+  if (isInvalid(validated)) {
+    throw new SubmissionError(validated);
+  }
+  if (submit) {
+    return submit(formValues);
+  }
+};
+
 export default {};
