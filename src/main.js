608f0494f3f0113df38fd3b0cbffc08de8029be4
sckoh <soochengkoh@gmail.com>
2019-07-06 12:21:12 +0800

    added normalizeDecimal

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { SubmissionError } from 'redux-form';
 import defaultShouldError from 'redux-form/lib/defaultShouldError';
+import Big from 'big.js';
 
 export const isInvalid = (errors = {}) => !!Object.keys(errors).length;
 
@@ -23,6 +24,22 @@ export const shouldError = customShouldError => (config) => {
     return true;
   }
   return false;
+};
+
+export const normalizeDecimal = decimalPoint => (value) => {
+  if (!value || !value.length || value[value.length - 1] === '.') {
+    return value;
+  }
+  // eslint-disable-next-line no-restricted-globals
+  if (!isNaN(value)) {
+    const factor = decimalPoint > 0 ? Big(10).pow(decimalPoint) : 1;
+    return Big(value)
+      .times(factor)
+      .round(0, 0)
+      .div(factor)
+      .valueOf();
+  }
+  return value;
 };
 
 export default {};
